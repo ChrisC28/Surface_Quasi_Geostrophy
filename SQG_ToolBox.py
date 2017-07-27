5034b5a4c095647965a5d21b6a1e04e26fcabba4
brajard <julien.brajard@inria.fr>
2017-07-27 16:19:24 +0200

    DEBUG flag

--- SQG_ToolBox.py
+++ SQG_ToolBox.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy import linalg
@@ -6,6 +7,9 @@ from scipy.fftpack import fft2,ifft2,fftshift,ifftshift,fftfreq
 import scipy.sparse as sparse  
 import scipy.sparse.linalg as sparse_linalg
 from past.builtins import xrange
+
+DEBUG = False
+
 
 class SQG:
     
@@ -243,9 +247,10 @@ class SQG:
                                 FT_dyn_height[iY,iX]-self.SQG_streamfunction_FFT[0,iY,iX],-self.SQG_streamfunction_FFT[self.nZ-1,iY,iX]+0.0j)
                  
                 #Reconstruct the interior streamfunction in wavenumber space
-                print 'barotropic baroclinic expansion coeffs'
-                print coeff0
-                print coeff1
+                if DEBUG:
+                    print ('barotropic baroclinic expansion coeffs')
+                    print (coeff0)
+                    print (coeff1)
                 if exapansion_coeffs==None:        
                     #if we are only using two modes, then the method reverts to
                     #the intSQG method proposed by Wang et al. (2013) where the
@@ -254,10 +259,12 @@ class SQG:
                     FT_interior_streamfunction[:,iY,iX] =coeff0*normal_modes[:,0] + coeff1*normal_modes[:,1]
                 else:
                     FT_interior_streamfunction[:,iY,iX] =coeff0*normal_modes[:,0] + coeff1*normal_modes[:,1] 
-                    print '====='
-                    print n_higher_modes
+                    if DEBUG:
+                        print ('=====')
+                        print (n_higher_modes)
                     for i_mode in range(0,n_higher_modes):
-                        print i_mode+2
+                        if DEBUG:
+                            print (i_mode+2)
                         FT_interior_streamfunction[:,iY,iX] = FT_interior_streamfunction[:,iY,iX] +   exapansion_coeffs_mirror[i_mode,iY,iX]*normal_modes[:,i_mode+2]   
                                                                   
         #Inverse Fourier Transform to convert back to the physical domain
